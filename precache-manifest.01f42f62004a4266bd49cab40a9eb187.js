5f175495ca92e61d8ba1d99654547ea9d5385881
andicuevasp <andicuevasp@gmail.com>
2021-09-10 17:44:50 +0200

    Updates

---
+++ precache-manifest.01f42f62004a4266bd49cab40a9eb187.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "396a32a8ed2741f6ac7f",
+    "url": "/name-tag-generator/static/css/main.0462215f.chunk.css"
+  },
+  {
+    "revision": "396a32a8ed2741f6ac7f",
+    "url": "/name-tag-generator/static/js/main.52cc7eda.chunk.js"
+  },
+  {
+    "revision": "3ae7b9e1728ccf5d7f50",
+    "url": "/name-tag-generator/static/js/runtime~main.0e060c98.js"
+  },
+  {
+    "revision": "45a7acfdace2e315d2d9",
+    "url": "/name-tag-generator/static/js/2.ee49d4bf.chunk.js"
+  },
+  {
+    "revision": "9ac720a9ebb1765adc609dd0588dee6a",
+    "url": "/name-tag-generator/index.html"
+  }
+];
